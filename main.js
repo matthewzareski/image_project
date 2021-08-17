e3cdc5b0f928f3e943490026098be720ef082952
matthewzareski <78160277+matthewzareski@users.noreply.github.com>
2021-08-17 12:52:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format :'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach(camera);
+
+
+function takesnapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/E29F3-W4J/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, result) {
+if (error) {
+    console.error(error);
+} else {
+    console.log(result);
+    document.getElementById("objname").innerHTML = result[0].label;
+    document.getElementById("percent").innerHTML = result[0].confidence.toFixed(3);
+
+}
+
+}
+    
+
